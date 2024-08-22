1386c1d1215318bafdc8b86cb8edb131bcabc8e2
CamiloR88 <camilordriguez88@gmail.com>
2024-08-21 21:27:57 -0500

    textContent Update

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const modal = document.querySelector(".popup");
-const openModal = document.querySelector(".profile__edit-btn");
-const saveModal = document.querySelector(".popup__save-btn");
-const closeModal = document.querySelector(".popup__close-btn");
+const modalOpen = document.querySelector(".profile__edit-btn");
+const modalForm = document.querySelector(".popup__form");
+const modalSave = document.querySelector(".popup__save-btn");
+const modalClose = document.querySelector(".popup__close-btn");
+const profileInfo = document.querySelector(".profile__info");
+const profileName = document.querySelector(".profile__name");
+const profilePosition = document.querySelector(".profile__position");
 
-function handleCloseModal() {
-  modal.setAttribute("style", "display: none");
+function handleModalClose() {
+  modal.close();
 }
 
-openModal.addEventListener("click", () => {
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  let modalInputName = document.querySelector("#name");
+  let modalInputAbout = document.querySelector("#about");
+
+  profileName.textContent = modalInputName.value;
+  profilePosition.textContent = modalInputAbout.value;
+  handleModalClose();
+}
+modalOpen.addEventListener("click", () => {
   modal.showModal();
 });
-closeModal.addEventListener("click", handleCloseModal);
+modalClose.addEventListener("click", handleModalClose);
+
+modalSave.addEventListener("click", handleFormSubmit);
