e8cbca5d99c3a09a5fa62a857049cab861a9d44e
CamiloR88 <camilordriguez88@gmail.com>
2024-09-02 23:56:11 -0500

    Agregar nueva tarjeta

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-const modal = document.querySelector(".popup");
-const modalOpen = document.querySelector(".profile__edit-btn");
-const modalForm = document.querySelector(".popup__form");
-const modalSave = document.querySelector(".popup__save-btn");
-const modalClose = document.querySelector(".popup__close-btn");
+//imagenes
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+//popup del perfil
+const profileModal = document.querySelector("#popup-profile");
+const profileModalOpen = document.querySelector(".profile__edit-btn");
+const profileModalSave = document.querySelector(".popup__save-btn");
+const profielModalClose = document.querySelector(".popup__close-btn");
 const profileInfo = document.querySelector(".profile__info");
 const profileName = document.querySelector(".profile__name");
 const profilePosition = document.querySelector(".profile__position");
+//popup para agregar imagenes
+const cardModal = document.querySelector("#add-card");
+const cardModalOpen = document.querySelector(".profile__add-btn");
+const cardModalSave = document.querySelector("#s-btn");
+const cardModalClose = document.querySelector("#c-btn");
+const cardsContainer = document.querySelector(".elements__container");
 
 function handleModalClose() {
-  modal.close();
+  profileModal.close();
+  cardModal.close();
 }
 
 function handleFormSubmit(evt) {
@@ -20,9 +54,40 @@ function handleFormSubmit(evt) {
   profilePosition.textContent = modalInputAbout.value;
   handleModalClose();
 }
-modalOpen.addEventListener("click", () => {
-  modal.showModal();
-});
-modalClose.addEventListener("click", handleModalClose);
 
-modalSave.addEventListener("click", handleFormSubmit);
+function handleCardAdd(evt) {}
+
+profileModalOpen.addEventListener("click", () => {
+  profileModal.showModal();
+});
+cardModalOpen.addEventListener("click", () => {
+  cardModal.showModal();
+});
+profielModalClose.addEventListener("click", handleModalClose);
+profileModalSave.addEventListener("click", handleFormSubmit);
+
+cardModalClose.addEventListener("click", handleModalClose);
+
+//function conectar el template y hacer una tarjeta nueva
+
+function addCard(cardTitleValue, cardUrlValue) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate
+    .querySelector(".elements__element")
+    .cloneNode(true);
+
+  cardElement.querySelector(".element__name").textContent = cardTitleValue;
+  cardElement.querySelector(".element__img").src = cardUrlValue;
+
+  cardsContainer.prepend(cardElement);
+}
+
+cardModalSave.addEventListener("click", function () {
+  let cardTitle = document.querySelector("#title");
+  let cardUrl = document.querySelector("#url");
+
+  addCard(cardTitle.value, cardUrl.value);
+
+  cardTitle.value = "";
+  cardUrl.value = "";
+});
