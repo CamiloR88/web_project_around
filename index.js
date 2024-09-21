a6588af362406efe134884e5e400bb1f0928b744
CamiloR88 <camilordriguez88@gmail.com>
2024-09-21 08:18:30 -0500

    solo faltan unos detalles

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //importacion
-//import {} from "./validate.js";
+import { enableValidation } from "./validate.js";
 
 //Cards
 const initialCards = [
@@ -72,9 +72,30 @@ cardModalOpen.addEventListener("click", () => {
 });
 profielModalClose.addEventListener("click", handleModalClose);
 profileModalSave.addEventListener("click", handleFormSubmit);
+profileModal.addEventListener("dblclick", (evt) => {
+  console.log(evt.target);
+  if (evt.target === profileModal) {
+    profileModal.close();
+  }
+});
 
 cardModalClose.addEventListener("click", handleModalClose);
+
+cardModal.addEventListener("dblclick", (evt) => {
+  console.log(evt.target);
+  if (evt.target === cardModal) {
+    cardModal.close();
+  }
+});
+
 imageModalClose.addEventListener("click", handleModalClose);
+
+imageModal.addEventListener("dblclick", (evt) => {
+  console.log(evt.target);
+  if (evt.target === imageModal) {
+    imageModal.close();
+  }
+});
 
 //function conectar el template y hacer una tarjeta nueva
 
@@ -122,78 +143,3 @@ cardModalSave.addEventListener("click", function () {
 initialCards.forEach(function (card) {
   addCard(card.name, card.link);
 });
-
-//validacion(beta)******************************validacion(beta)
-
-//activa y desactiva la clase error en los elementos
-const showInputError = (formElement, inputElement, errorMessage) => {
-  console.log(inputElement.id);
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add("popup__input_type_error");
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("popup__input-error_active");
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove("popup__input_type_error");
-  errorElement.classList.remove("popup__input-error_active");
-  errorElement.textContent = "";
-};
-//chequea la validez del form y manda a ejecutar (showInputError || hideInputError) segun corresponda
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-//comprueva si algun input tiene algun valor incorrecto
-const hasInvalidInput = (inputList) => {
-  //some devuleve true o flase
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-};
-//intercambia el estado del boton activo y desactivo dependiendo de la validez de los inputs
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add("popup__save-btn_inactive");
-    buttonElement.disabled = true;
-  } else {
-    buttonElement.classList.remove("popup__save-btn_inactive");
-    buttonElement.disabled = false;
-  }
-};
-
-const setEventListeners = (formElement) => {
-  //hace un arry con todos los inputs del formulario
-  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
-
-  const buttonElement = formElement.querySelector(".popup__save-btn");
-
-  toggleButtonState(inputList, buttonElement);
-  //itera por el array de inputs chequeando su validez para asi activar el boton si es valido
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener("input", function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-const enableValidation = () => {
-  //hace un arry con los form
-  const formList = Array.from(document.querySelectorAll(".popup__form"));
-
-  // crea el parametro formElement que luego usaremos en otras funciones
-  formList.forEach((formElement) => {
-    formElement.addEventListener("submit", function (evt) {
-      evt.preventDefault();
-    });
-
-    setEventListeners(formElement);
-  });
-};
-
-enableValidation();
--- validate.js
+++ validate.js
@@ -1,46 +1,51 @@
+//validacion
+
 //activa y desactiva la clase error en los elementos
-export const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage) => {
+  console.log(inputElement.id);
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add("popup__input_type_error");
   errorElement.textContent = errorMessage;
-  errorElement.classList.add("form__input-error_active");
+  errorElement.classList.add("popup__input-error_active");
 };
 
-export const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove("form__input_type_error");
-  errorElement.classList.remove("form__input-error_active");
+  inputElement.classList.remove("popup__input_type_error");
+  errorElement.classList.remove("popup__input-error_active");
   errorElement.textContent = "";
 };
-//chequea la validez del form y manda a ejuctar (showInputError || hideInputError) segun corresponda
-export const checkInputValidity = (formElement, inputElement) => {
+//chequea la validez del form y manda a ejecutar (showInputError || hideInputError) segun corresponda
+const checkInputValidity = (formElement, inputElement) => {
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
     hideInputError(formElement, inputElement);
   }
 };
-//comprueva si el input tiene algun valor incorrecto
-export const hasInvalidInput = (inputList) => {
+//comprueva si algun input tiene algun valor incorrecto
+const hasInvalidInput = (inputList) => {
   //some devuleve true o flase
   return inputList.some((inputElement) => {
     return !inputElement.validity.valid;
   });
 };
 //intercambia el estado del boton activo y desactivo dependiendo de la validez de los inputs
-export const toggleButtonState = (inputList, buttonElement) => {
+const toggleButtonState = (inputList, buttonElement) => {
   if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add("button_inactive");
+    buttonElement.classList.add("popup__save-btn_inactive");
+    buttonElement.disabled = true;
   } else {
-    buttonElement.classList.remove("button_inactive");
+    buttonElement.classList.remove("popup__save-btn_inactive");
+    buttonElement.disabled = false;
   }
 };
 
-export const setEventListeners = (formElement) => {
+const setEventListeners = (formElement) => {
   //hace un arry con todos los inputs del formulario
   const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
-  console.log(inputList);
-  const buttonElement = formElement.querySelector(".form__submit");
+
+  const buttonElement = formElement.querySelector(".popup__save-btn");
 
   toggleButtonState(inputList, buttonElement);
   //itera por el array de inputs chequeando su validez para asi activar el boton si es valido
@@ -55,6 +60,8 @@ export const setEventListeners = (formElement) => {
 export const enableValidation = () => {
   //hace un arry con los form
   const formList = Array.from(document.querySelectorAll(".popup__form"));
+
+  // crea el parametro formElement que luego usaremos en otras funciones
   formList.forEach((formElement) => {
     formElement.addEventListener("submit", function (evt) {
       evt.preventDefault();
